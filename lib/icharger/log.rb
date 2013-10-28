4befcac1e9b67f3641f749db2ae50a70e9fe672f
Nick Veys <nick@codelever.com>
2013-10-28 00:29:04 -0500

    Adding generated files

---
+++ lib/icharger/log.rb
@@ -0,0 +1,7 @@
+require "icharger/log/version"
+
+module Icharger
+  module Log
+    # Your code goes here...
+  end
+end
---
+++ lib/icharger/log/version.rb
@@ -0,0 +1,5 @@
+module Icharger
+  module Log
+    VERSION = "0.0.1"
+  end
+end
